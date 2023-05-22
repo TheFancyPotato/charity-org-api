8ebb9fa5b7d9c1dbe463d6c72628ace938def0a6
Mahdi Mohammed <48333830+mahdi-mk@users.noreply.github.com>
2023-05-22 18:45:31 +0300

    Add superadmin user seeder

---
+++ database/seeders/SuperadminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\UserRole;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class SuperadminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Superadmin',
+            'email' => 'superadmin@example.com',
+            'password' => Hash::make('12345678'),
+            'role' => UserRole::Superadmin,
+        ]);
+    }
+}
